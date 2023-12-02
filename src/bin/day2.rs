d02e816d1c5ceb4bf3f8252d8c95aa246251e816
Daniel Connelly <dhconnelly@gmail.com>
2023-12-02 13:20:05 +0100

    day2: cleanups

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -4,4 +4,5 @@ fn main() {
     let path = std::env::args().skip(1).next().unwrap();
     let data = std::fs::read_to_string(&path).unwrap();
     println!("{}", day2::part1(&data));
+    println!("{}", day2::part2(&data));
 }
--- src/day2.rs
+++ src/day2.rs
@@ -1,25 +1,8 @@
 use core::iter::Iterator;
 use core::str::Split;
-
-use libc_print::std_name::println;
 use regex::Regex;
 
-#[derive(Debug)]
-struct Outcome {
-    r: i64,
-    g: i64,
-    b: i64,
-}
-
-impl Outcome {
-    fn new(r: i64, g: i64, b: i64) -> Self {
-        Self { r, g, b }
-    }
-}
-
-fn parse(pat: &Regex, line: &str) -> (usize, Outcome) {
-    (0, Outcome::new(0, 0, 0))
-}
+struct Outcome(i64, i64, i64);
 
 struct Outcomes<'a, 'b> {
     pat: &'b Regex,
@@ -28,15 +11,16 @@ struct Outcomes<'a, 'b> {
 
 impl Iterator for Outcomes<'_, '_> {
     type Item = Outcome;
+
     fn next(&mut self) -> Option<Self::Item> {
         let seg = self.segs.next()?;
-        let mut outcome = Outcome::new(0,0,0);
+        let mut outcome = Outcome(0, 0, 0);
         for (_, [amt, col]) in self.pat.captures_iter(seg).map(|c| c.extract()) {
             let amt = amt.parse().unwrap();
             match col {
-                "red" => outcome.r = amt,
-                "green" => outcome.g = amt,
-                "blue" => outcome.b = amt,
+                "red" => outcome.0 = amt,
+                "green" => outcome.1 = amt,
+                "blue" => outcome.2 = amt,
                 _ => panic!("invalid color"),
             }
         }
@@ -58,51 +42,86 @@ impl Parser {
 
     fn parse<'a, 'b>(&'b self, line: &'a str) -> (usize, Outcomes<'a, 'b>) {
         let (game, outcomes) = line.split_once(':').unwrap();
-        let game_caps = self.game_pat.captures(game).unwrap();
-        let (_, [id]) = game_caps.extract();
-        let id = id.parse().unwrap();
-        let segs = outcomes.split(';');
-        (id, Outcomes { segs, pat: &self.outcome_pat })
+        let (_, [id]) = self.game_pat.captures(game).unwrap().extract();
+        let outcome = Outcomes { segs: outcomes.split(';'), pat: &self.outcome_pat };
+        (id.parse().unwrap(), outcome)
     }
 }
 
 fn can_fit(into: &Outcome, from: &Outcome) -> bool {
-    from.r <= into.r && from.g <= into.g && from.b <= into.b
+    from.0 <= into.0 && from.1 <= into.1 && from.2 <= into.2
 }
 
 pub fn part1(input: &str) -> usize {
-    let want = Outcome::new(12, 13, 14);
-    let p = Parser::new();
-    let mut sum = 0;
-    for (id, outcomes) in input.lines().map(|line| p.parse(line)) {
-        let mut ok = true;
-        for outcome in outcomes {
-            if !can_fit(&want, &outcome) {
-                ok = false;
-                break;
-            }
+    let parser = Parser::new();
+    let available = Outcome(12, 13, 14);
+    let all_games = input.lines().map(|line| parser.parse(line));
+    let possible_games = all_games.flat_map(|(id, mut outcomes)| {
+        if outcomes.all(|outcome| can_fit(&available, &outcome)) {
+            Some(id)
+        } else {
+            None
         }
-        if ok {
-            sum += id;
-        }
+    });
+    possible_games.sum()
+}
+
+fn power(outcomes: impl Iterator<Item = Outcome>) -> i64 {
+    let mut min = Outcome(0, 0, 0);
+    for outcome in outcomes {
+        min.0 = min.0.max(outcome.0);
+        min.1 = min.1.max(outcome.1);
+        min.2 = min.2.max(outcome.2);
     }
-    sum
+    min.0 * min.1 * min.2
+}
+
+pub fn part2(input: &str) -> i64 {
+    let p = Parser::new();
+    let all_games = input.lines().map(|line| p.parse(line));
+    let powers = all_games.map(|(_, outcomes)| power(outcomes));
+    powers.sum()
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
 
-    #[test]
-    fn test() {
-        let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    const INPUT: &str = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
 Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
 ";
-        let output = part1(input);
-        let expected = 8;
-        assert_eq!(output, expected);
+
+    #[test]
+    fn test1() {
+        assert_eq!(part1(INPUT), 8);
+    }
+
+    #[test]
+    fn test2_1() {
+        let mut lines = INPUT.lines();
+        let p = Parser::new();
+        assert_eq!(power(p.parse(lines.next().unwrap()).1), 48);
+        assert_eq!(power(p.parse(lines.next().unwrap()).1), 12);
+        assert_eq!(power(p.parse(lines.next().unwrap()).1), 1560);
+        assert_eq!(power(p.parse(lines.next().unwrap()).1), 630);
+        assert_eq!(power(p.parse(lines.next().unwrap()).1), 36);
+        assert_eq!(lines.next(), None);
+    }
+
+    #[test]
+    fn test2_2() {
+        assert_eq!(part2(INPUT), 2286);
+    }
+
+    #[test]
+    fn test_solution() {
+        const INPUT: &str = include_str!("../inputs/day2.txt");
+        let actual1 = part1(INPUT);
+        assert_eq!(2204, actual1);
+        let actual2 = part2(INPUT);
+        assert_eq!(71036, actual2);
     }
 }
