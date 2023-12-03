fae10759aabed778a7bb19e3f98646646881f547
Daniel Connelly <dhconnelly@gmail.com>
2023-12-03 10:26:36 +0100

    day3: unify

--- src/day3.rs
+++ src/day3.rs
@@ -1,91 +1,83 @@
-use crate::ascii::IndexAscii;
 use core::iter::Iterator;
 use regex::Regex;
 
-fn is_symbol(b: &char) -> bool {
-    !b.is_ascii_digit() && b != &'.'
-}
-
-fn find_symbol_in_cur(cur: &str, col_start: usize, col_end: usize) -> Option<char> {
-    if col_start > 0 && is_symbol(&cur.ascii_at(col_start - 1)) {
-        Some(cur.ascii_at(col_start - 1))
-    } else if col_end < cur.len() && is_symbol(&cur.ascii_at(col_end)) {
-        Some(cur.ascii_at(col_end))
-    } else {
-        None
-    }
-}
-
-fn find_symbol_in_adj(
-    line: &str,
-    mut col_start: usize,
-    mut col_end: usize,
-) -> Option<char> {
-    if col_start > 0 {
-        col_start -= 1;
-    }
-    if col_end < line.len() - 1 {
-        col_end += 1;
-    }
-    let line = &line[col_start..col_end];
-    line.chars().find(is_symbol)
-}
-
-fn find_adjacent_symbol(
+fn find_adjacent_in_window(
+    pat: &Regex,
     above: Option<&str>,
     cur: &str,
     below: Option<&str>,
     start: usize,
     end: usize,
-) -> Option<char> {
-    if let Some(sym) = find_symbol_in_cur(cur, start, end) {
-        return Some(sym);
+    mut f: impl FnMut(&str),
+) {
+    for cap in pat.captures_iter(cur).map(|cap| cap.get(0).unwrap()) {
+        let (cap_start, cap_end) = (cap.start(), cap.end());
+        if cap_start == end || cap_end == start {
+            f(cap.as_str());
+        }
     }
-    above
-        .and_then(|above| find_symbol_in_adj(above, start, end))
-        .or_else(|| below.and_then(|below| find_symbol_in_adj(below, start, end)))
+    for line in [above, below].into_iter().flatten() {
+        for cap in pat.captures_iter(line).map(|cap| cap.get(0).unwrap()) {
+            let (cap_start, cap_end) = (cap.start(), cap.end());
+            if cap_start <= end && cap_end >= start {
+                f(cap.as_str());
+            }
+        }
+    }
+}
+
+fn process_windows(
+    input: &str,
+    mut f: impl FnMut(Option<&str>, &str, Option<&str>),
+) {
+    // get sliding windows of lines (above, cur, below)
+    let mut lines = input.lines();
+    let mut above = lines.next().unwrap();
+
+    // first line
+    let maybe_cur = lines.next();
+    if let Some(line) = maybe_cur {
+        f(None, above, Some(line));
+    } else {
+        return;
+    }
+
+    // middle lines
+    let mut cur = maybe_cur.unwrap();
+    for line in lines {
+        let below = line;
+        f(Some(above), cur, Some(below));
+        (above, cur) = (cur, below);
+    }
+
+    // last line
+    f(Some(above), cur, None);
 }
 
 fn line_sum(
-    pat: &Regex,
+    num_pat: &Regex,
+    sym_pat: &Regex,
     above: Option<&str>,
     cur: &str,
     below: Option<&str>,
 ) -> i64 {
     let mut sum = 0;
-    for num_cap in pat.captures_iter(cur).map(|cap| cap.get(1).unwrap()) {
+    for num_cap in num_pat.captures_iter(cur).map(|cap| cap.get(0).unwrap()) {
         let (start, end) = (num_cap.start(), num_cap.end());
-        if let Some(_) = find_adjacent_symbol(above, cur, below, start, end) {
-            let num: i64 = num_cap.as_str().parse().unwrap();
-            sum += num;
-        }
+        find_adjacent_in_window(sym_pat, above, cur, below, start, end, |_| {
+            sum += num_cap.as_str().parse::<i64>().unwrap();
+        });
     }
     sum
 }
 
 pub fn part1(input: &str) -> i64 {
-    let pat = Regex::new(r"(\d+)").unwrap();
+    let num_pat = Regex::new(r"\d+").unwrap();
+    let sym_pat = Regex::new(r"[^.\d]").unwrap();
     let mut sum = 0;
-
-    // get sliding windows of lines (above, cur, below)
-    let mut lines = input.lines();
-    let mut above = lines.next().unwrap();
-    let maybe_cur = lines.next();
-    if let Some(line) = maybe_cur {
-        sum += line_sum(&pat, None, above, Some(line));
-    } else {
-        return sum;
-    }
-
-    let mut cur = maybe_cur.unwrap();
-    for line in lines {
-        let below = line;
-        sum += line_sum(&pat, Some(above), cur, Some(below));
-        (above, cur) = (cur, below);
-    }
-
-    sum += line_sum(&pat, Some(above), cur, None);
-
+    process_windows(input, |above, cur, below| {
+        sum += line_sum(&num_pat, &sym_pat, above, cur, below);
+    });
     sum
 }
 
@@ -99,44 +91,13 @@ fn line_gear_ratio(
     for (i, _) in cur.chars().enumerate().filter(|(_, ch)| *ch == '*') {
         let mut count = 0;
         let mut ratio = 1;
-
-        for num_cap in pat.captures_iter(cur).map(|cap| cap.get(1).unwrap()) {
+        find_adjacent_in_window(pat, above, cur, below, i, i + 1, |cap| {
             if count < 2 {
-                let (start, end) = (num_cap.start(), num_cap.end());
-                if end == i || start == i + 1 {
-                    let num: i64 = num_cap.as_str().parse().unwrap();
-                    ratio *= num;
-                    count += 1;
-                }
+                let num: i64 = cap.parse().unwrap();
+                ratio *= num;
+                count += 1;
             }
-        }
-
-        if let Some(above) = above {
-            for num_cap in pat.captures_iter(above).map(|cap| cap.get(1).unwrap()) {
-                if count < 2 {
-                    let (start, end) = (num_cap.start(), num_cap.end());
-                    if start <= i + 1 && end >= i {
-                        let num: i64 = num_cap.as_str().parse().unwrap();
-                        ratio *= num;
-                        count += 1;
-                    }
-                }
-            }
-        }
-
-        if let Some(below) = below {
-            for num_cap in pat.captures_iter(below).map(|cap| cap.get(1).unwrap()) {
-                if count < 2 {
-                    let (start, end) = (num_cap.start(), num_cap.end());
-                    if start <= i + 1 && end >= i {
-                        let num: i64 = num_cap.as_str().parse().unwrap();
-                        ratio *= num;
-                        count += 1;
-                    }
-                }
-            }
-        }
-
+        });
         if count == 2 {
             sum += ratio;
         }
@@ -145,28 +106,11 @@ fn line_gear_ratio(
 }
 
 pub fn part2(input: &str) -> i64 {
-    let pat = Regex::new(r"(\d+)").unwrap();
+    let pat = Regex::new(r"\d+").unwrap();
     let mut sum = 0;
-
-    // get sliding windows of lines (above, cur, below)
-    let mut lines = input.lines();
-    let mut above = lines.next().unwrap();
-    let maybe_cur = lines.next();
-    if let Some(line) = maybe_cur {
-        sum += line_gear_ratio(&pat, None, above, Some(line));
-    } else {
-        return sum;
-    }
-
-    let mut cur = maybe_cur.unwrap();
-    for line in lines {
-        let below = line;
-        sum += line_gear_ratio(&pat, Some(above), cur, Some(below));
-        (above, cur) = (cur, below);
-    }
-
-    sum += line_gear_ratio(&pat, Some(above), cur, None);
-
+    process_windows(input, |above, cur, below| {
+        sum += line_gear_ratio(&pat, above, cur, below);
+    });
     sum
 }
 
@@ -195,5 +139,6 @@ mod test {
     fn test2() {
         let input = include_str!("../inputs/day3.txt");
         assert_eq!(part1(input), 556057);
+        assert_eq!(part2(input), 82824352);
     }
 }
