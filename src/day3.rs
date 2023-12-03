085b75f8eda98082086ea75f572cafb8c8b6160e
Daniel Connelly <dhconnelly@gmail.com>
2023-12-03 09:12:47 +0100

    day3: done but very ugly

--- src/day3.rs
+++ src/day3.rs
@@ -1,6 +1,5 @@
 use crate::ascii::IndexAscii;
 use core::iter::Iterator;
-use libc_print::std_name::println;
 use regex::Regex;
 
 fn is_symbol(b: &char) -> bool {
@@ -54,10 +53,9 @@ fn line_sum(
     below: Option<&str>,
 ) -> i64 {
     let mut sum = 0;
-    for cap in pat.captures_iter(cur) {
-        let num_cap = cap.get(1).unwrap();
+    for num_cap in pat.captures_iter(cur).map(|cap| cap.get(1).unwrap()) {
         let (start, end) = (num_cap.start(), num_cap.end());
-        if let Some(sym) = find_adjacent_symbol(above, cur, below, start, end) {
+        if let Some(_) = find_adjacent_symbol(above, cur, below, start, end) {
             let num: i64 = num_cap.as_str().parse().unwrap();
             sum += num;
         }
@@ -73,14 +71,13 @@ pub fn part1(input: &str) -> i64 {
     let mut lines = input.lines();
     let mut above = lines.next().unwrap();
     let maybe_cur = lines.next();
-    let mut cur;
     if let Some(line) = maybe_cur {
         sum += line_sum(&pat, None, above, Some(line));
-        cur = line;
     } else {
         return sum;
     }
 
+    let mut cur = maybe_cur.unwrap();
     for line in lines {
         let below = line;
         sum += line_sum(&pat, Some(above), cur, Some(below));
@@ -92,8 +89,85 @@ pub fn part1(input: &str) -> i64 {
     sum
 }
 
-pub fn part2(input: &str) -> usize {
-    0
+fn line_gear_ratio(
+    pat: &Regex,
+    above: Option<&str>,
+    cur: &str,
+    below: Option<&str>,
+) -> i64 {
+    let mut sum = 0;
+    for (i, _) in cur.chars().enumerate().filter(|(_, ch)| *ch == '*') {
+        let mut count = 0;
+        let mut ratio = 1;
+
+        for num_cap in pat.captures_iter(cur).map(|cap| cap.get(1).unwrap()) {
+            if count < 2 {
+                let (start, end) = (num_cap.start(), num_cap.end());
+                if end == i || start == i + 1 {
+                    let num: i64 = num_cap.as_str().parse().unwrap();
+                    ratio *= num;
+                    count += 1;
+                }
+            }
+        }
+
+        if let Some(above) = above {
+            for num_cap in pat.captures_iter(above).map(|cap| cap.get(1).unwrap()) {
+                if count < 2 {
+                    let (start, end) = (num_cap.start(), num_cap.end());
+                    if start <= i + 1 && end >= i {
+                        let num: i64 = num_cap.as_str().parse().unwrap();
+                        ratio *= num;
+                        count += 1;
+                    }
+                }
+            }
+        }
+
+        if let Some(below) = below {
+            for num_cap in pat.captures_iter(below).map(|cap| cap.get(1).unwrap()) {
+                if count < 2 {
+                    let (start, end) = (num_cap.start(), num_cap.end());
+                    if start <= i + 1 && end >= i {
+                        let num: i64 = num_cap.as_str().parse().unwrap();
+                        ratio *= num;
+                        count += 1;
+                    }
+                }
+            }
+        }
+
+        if count == 2 {
+            sum += ratio;
+        }
+    }
+    sum
+}
+
+pub fn part2(input: &str) -> i64 {
+    let pat = Regex::new(r"(\d+)").unwrap();
+    let mut sum = 0;
+
+    // get sliding windows of lines (above, cur, below)
+    let mut lines = input.lines();
+    let mut above = lines.next().unwrap();
+    let maybe_cur = lines.next();
+    if let Some(line) = maybe_cur {
+        sum += line_gear_ratio(&pat, None, above, Some(line));
+    } else {
+        return sum;
+    }
+
+    let mut cur = maybe_cur.unwrap();
+    for line in lines {
+        let below = line;
+        sum += line_gear_ratio(&pat, Some(above), cur, Some(below));
+        (above, cur) = (cur, below);
+    }
+
+    sum += line_gear_ratio(&pat, Some(above), cur, None);
+
+    sum
 }
 
 #[cfg(test)]
@@ -114,5 +188,12 @@ mod test {
 .664.598..
 ";
         assert_eq!(part1(input), 4361);
+        assert_eq!(part2(input), 467835);
+    }
+
+    #[test]
+    fn test2() {
+        let input = include_str!("../inputs/day3.txt");
+        assert_eq!(part1(input), 556057);
     }
 }
