db75e759f07a46991e67e96536454e2beebf68a6
Daniel Connelly <dhconnelly@gmail.com>
2023-12-03 12:28:59 +0100

    day3: more concise

--- src/day3.rs
+++ src/day3.rs
@@ -3,6 +3,20 @@ use regex::Regex;
 
 type LineWindow<'a> = (Option<&'a str>, &'a str, Option<&'a str>);
 
+struct Windows<'a> {
+    lines: Lines<'a>,
+    buf: [Option<&'a str>; 2],
+}
+
+impl<'a> Iterator for Windows<'a> {
+    type Item = LineWindow<'a>;
+    fn next(&mut self) -> Option<Self::Item> {
+        let window = (self.buf[0].take(), self.buf[1].take()?, self.lines.next());
+        self.buf = [Some(window.1), window.2];
+        Some(window)
+    }
+}
+
 trait Windowable<'a> {
     fn windows(self) -> Windows<'a>;
 }
@@ -14,42 +28,22 @@ impl<'a> Windowable<'a> for Lines<'a> {
     }
 }
 
-struct Windows<'a> {
-    lines: Lines<'a>,
-    buf: [Option<&'a str>; 2],
-}
-
-impl<'a> Iterator for Windows<'a> {
-    type Item = LineWindow<'a>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let above = self.buf[0].take();
-        let cur = self.buf[1].take()?;
-        let below = self.lines.next();
-        let item = (above, cur, below);
-        self.buf = [Some(cur), below];
-        Some(item)
-    }
-}
-
 fn find_adjacent_in_window(
     pat: &Regex,
-    window: LineWindow,
+    (above, cur, below): LineWindow,
     around_range: (usize, usize),
     mut f: impl FnMut(&str),
 ) {
-    let (start, end) = around_range;
-    let (above, cur, below) = window;
     for cap in pat.captures_iter(cur).map(|cap| cap.get(0).unwrap()) {
         let (cap_start, cap_end) = (cap.start(), cap.end());
-        if cap_start == end || cap_end == start {
+        if cap_start == around_range.1 || cap_end == around_range.0 {
             f(cap.as_str());
         }
     }
     for line in [above, below].into_iter().flatten() {
         for cap in pat.captures_iter(line).map(|cap| cap.get(0).unwrap()) {
             let (cap_start, cap_end) = (cap.start(), cap.end());
-            if cap_start <= end && cap_end >= start {
+            if cap_start <= around_range.1 && cap_end >= around_range.0 {
                 f(cap.as_str());
             }
         }
@@ -57,9 +51,8 @@ fn find_adjacent_in_window(
 }
 
 fn line_symbol_sum(num_pat: &Regex, sym_pat: &Regex, window: LineWindow) -> i64 {
-    let (_, cur, _) = window;
     let mut sum = 0;
-    for num_cap in num_pat.captures_iter(cur).map(|cap| cap.get(0).unwrap()) {
+    for num_cap in num_pat.captures_iter(window.1).map(|cap| cap.get(0).unwrap()) {
         let (start, end) = (num_cap.start(), num_cap.end());
         find_adjacent_in_window(sym_pat, window, (start, end), |_| {
             sum += num_cap.as_str().parse::<i64>().unwrap();
@@ -71,25 +64,17 @@ fn line_symbol_sum(num_pat: &Regex, sym_pat: &Regex, window: LineWindow) -> i64 
 pub fn part1(input: &str) -> i64 {
     let num_pat = Regex::new(r"\d+").unwrap();
     let sym_pat = Regex::new(r"[^.\d]").unwrap();
-    input
-        .lines()
-        .windows()
-        .map(|window| line_symbol_sum(&num_pat, &sym_pat, window))
-        .sum()
+    input.lines().windows().map(|w| line_symbol_sum(&num_pat, &sym_pat, w)).sum()
 }
 
 fn line_gear_sum(pat: &Regex, window: LineWindow) -> i64 {
     let (above, cur, below) = window;
     let mut sum = 0;
     for (i, _) in cur.chars().enumerate().filter(|(_, ch)| *ch == '*') {
-        let mut count = 0;
-        let mut ratio = 1;
+        let (mut count, mut ratio) = (0, 1);
         find_adjacent_in_window(pat, (above, cur, below), (i, i + 1), |cap| {
-            if count < 2 {
-                let num: i64 = cap.parse().unwrap();
-                ratio *= num;
-                count += 1;
-            }
+            ratio *= cap.parse::<i64>().unwrap();
+            count += 1;
         });
         if count == 2 {
             sum += ratio;
@@ -100,7 +85,7 @@ fn line_gear_sum(pat: &Regex, window: LineWindow) -> i64 {
 
 pub fn part2(input: &str) -> i64 {
     let pat = Regex::new(r"\d+").unwrap();
-    input.lines().windows().map(|window| line_gear_sum(&pat, window)).sum()
+    input.lines().windows().map(|w| line_gear_sum(&pat, w)).sum()
 }
 
 #[cfg(test)]
