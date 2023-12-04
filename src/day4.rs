ef99973bed6c7ee4a347af12ebf55c123280f1f8
Daniel Connelly <dhconnelly@gmail.com>
2023-12-04 08:10:44 +0100

    day4: part2

--- src/day4.rs
+++ src/day4.rs
@@ -12,34 +12,48 @@ fn parse(m: &Captures) -> u8 {
 }
 
 fn set_of(nums: &str) -> u128 {
-    NUM_RE.captures_iter(nums).fold(0, |acc, m| dbg!(acc | (1 << parse(&m))))
+    NUM_RE.captures_iter(nums).fold(0, |acc, m| acc | (1 << parse(&m)))
 }
 
 fn set_contains(set: u128, num: u8) -> bool {
     set & (1 << num) > 0
 }
 
-fn score_card(card: &str) -> i64 {
-    let (_, nums) = card.split_at(card.find(':').unwrap());
+fn num_wins(card: &str) -> (usize, usize) {
+    let (card_name, nums) = card.split_at(card.find(':').unwrap());
+    let i = parse(&NUM_RE.captures(card_name).unwrap());
     let (winning, have) = nums.split_at(nums.find('|').unwrap());
     let winning_set = set_of(winning);
     let num_wins = NUM_RE
         .captures_iter(have)
         .filter(|m| set_contains(winning_set, parse(m)))
         .count();
-    if num_wins == 0 {
+    (i.into(), num_wins)
+}
+
+fn score_wins(wins: usize) -> usize {
+    if wins == 0 {
         0
     } else {
-        1 << (num_wins - 1)
+        1 << wins - 1
     }
 }
 
-pub fn part1(input: &str) -> i64 {
-    input.lines().map(score_card).sum()
+pub fn part1(input: &str) -> usize {
+    input.lines().map(num_wins).map(|(_, wins)| score_wins(wins)).sum()
 }
 
-pub fn part2(input: &str) -> i64 {
-    0
+pub fn part2(input: &str) -> usize {
+    let mut card_counts = [0usize; 256];
+    let mut card_wins = [0usize; 256];
+    for (i, wins) in input.lines().map(num_wins) {
+        card_counts[i] += 1;
+        card_wins[i] = wins;
+        for j in 0..wins {
+            card_counts[i + j + 1] += card_counts[i];
+        }
+    }
+    card_counts.into_iter().sum()
 }
 
 #[cfg(test)]
@@ -57,5 +71,12 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
     #[test]
     fn test() {
         assert_eq!(part1(INPUT), 13);
+        assert_eq!(part2(INPUT), 30);
+    }
+
+    #[test]
+    fn test_real() {
+        let input = include_str!("../inputs/day4.txt");
+        assert_eq!(part1(input), 19855);
     }
 }
