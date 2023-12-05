1aeb35c2c050a2d47deb88177b12bf963eaf5ee0
Daniel Connelly <dhconnelly@gmail.com>
2023-12-05 23:10:15 +0100

    static_vec: rename wrapped array

--- src/static_vec.rs
+++ src/static_vec.rs
@@ -2,13 +2,13 @@ use core::ops::{Index, IndexMut};
 
 #[derive(Clone, Copy)]
 pub struct StaticVec<T: Default + Copy, const N: usize> {
-    ranges: [T; N],
+    data: [T; N],
     len: usize,
 }
 
 impl<T: Default + Copy, const N: usize> StaticVec<T, N> {
     pub fn push(&mut self, elem: T) {
-        self.ranges[self.len] = elem;
+        self.data[self.len] = elem;
         self.len += 1;
     }
 
@@ -20,20 +20,19 @@ impl<T: Default + Copy, const N: usize> StaticVec<T, N> {
 impl<T: Default + Copy, const N: usize> Index<usize> for StaticVec<T, N> {
     type Output = T;
     fn index(&self, index: usize) -> &Self::Output {
-        &self.ranges[index]
+        &self.data[index]
     }
 }
 
 impl<T: Default + Copy, const N: usize> IndexMut<usize> for StaticVec<T, N> {
     fn index_mut(&mut self, index: usize) -> &mut Self::Output {
-        &mut self.ranges[index]
+        &mut self.data[index]
     }
 }
 
 impl<T: Default + Copy, const N: usize> Default for StaticVec<T, N> {
     fn default() -> Self {
-        let ranges = [T::default(); N];
-        Self { ranges, len: 0 }
+        Self { data: [T::default(); N], len: 0 }
     }
 }
 
@@ -41,6 +40,6 @@ impl<T: Default + Copy, const N: usize> IntoIterator for StaticVec<T, N> {
     type Item = T;
     type IntoIter = <[T; N] as IntoIterator>::IntoIter;
     fn into_iter(self) -> Self::IntoIter {
-        self.ranges.into_iter()
+        self.data.into_iter()
     }
 }
