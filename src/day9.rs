02c23e5a04e09eb8e8dc8dfbd43ece3e522a31c4
Daniel Connelly <dhconnelly@gmail.com>
2023-12-09 06:45:28 +0100

    day9: part2

--- src/day9.rs
+++ src/day9.rs
@@ -1,20 +1,4 @@
-use libc_print::libc_println;
-
-use crate::static_vec::StaticVec;
-
-type Seq = StaticVec<i64, 32>;
-
-lazy_static! {
-    static ref ZERO: Seq = StaticVec::of(0);
-}
-
-fn diffs(seq: &Seq) -> Seq {
-    let mut diffs = Seq::empty();
-    for i in 1..seq.len() {
-        diffs.push(seq[i] - seq[i - 1]);
-    }
-    diffs
-}
+type Seq = crate::static_vec::StaticVec<i64, 32>;
 
 fn next(seq: Seq) -> i64 {
     if seq.iter().all(|x| *x == 0) {
@@ -22,6 +6,18 @@ fn next(seq: Seq) -> i64 {
     } else {
         seq[seq.len() - 1] + next(diffs(&seq))
     }
+}
+
+fn prev(seq: Seq) -> i64 {
+    if seq.iter().all(|x| *x == 0) {
+        0
+    } else {
+        seq[0] - prev(diffs(&seq))
+    }
+}
+
+fn diffs(seq: &Seq) -> Seq {
+    (1..seq.len()).map(|i| seq[i] - seq[i - 1]).collect()
 }
 
 fn parse(line: &str) -> Seq {
@@ -33,7 +29,7 @@ pub fn part1(input: &str) -> i64 {
 }
 
 pub fn part2(input: &str) -> i64 {
-    0
+    input.lines().map(parse).map(prev).sum()
 }
 
 #[cfg(test)]
@@ -47,13 +43,13 @@ mod test {
 10 13 16 21 30 45
 ";
         assert_eq!(part1(input), 114);
-        assert_eq!(part2(input), 0);
+        assert_eq!(part2(input), 2);
     }
 
     #[test]
     fn test_real() {
         let input = include_str!("../inputs/day9.txt");
-        assert_eq!(part1(input), 0);
-        assert_eq!(part2(input), 0);
+        assert_eq!(part1(input), 1581679977);
+        assert_eq!(part2(input), 889);
     }
 }
