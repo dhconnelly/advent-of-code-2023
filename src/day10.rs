8e570bb85d1dd45aa256987321fc3a973c2e5bb4
Daniel Connelly <dhconnelly@gmail.com>
2023-12-10 14:05:28 +0100

    day10: prep for part 2

--- src/day10.rs
+++ src/day10.rs
@@ -96,7 +96,7 @@ type Set = StaticVec<Pt2, 32768>;
 // TODO: add a range-contains
 type Queue = StaticVec<(Pt2, i32), 32768>;
 
-fn loop_max(grid: &Grid, start: Pt2) -> Option<i32> {
+fn find_loop(grid: &Grid, start: Pt2) -> Option<Set> {
     let mut q = Queue::empty();
     let mut v = Set::empty();
     q.push((start, 0));
@@ -108,7 +108,8 @@ fn loop_max(grid: &Grid, start: Pt2) -> Option<i32> {
         let nbrs = connections(&grid, cur);
         for nbr in nbrs {
             if q.contains(&(nbr, dist + 1)) {
-                return Some(dist + 1);
+                v.push(nbr);
+                return Some(v);
             }
             if v.contains(&nbr) {
                 continue;
@@ -123,10 +124,14 @@ fn loop_max(grid: &Grid, start: Pt2) -> Option<i32> {
 pub fn part1(input: &str) -> i32 {
     let grid = parse(input);
     let start = find_start(&grid).unwrap();
-    loop_max(&grid, start).unwrap()
+    let lupe = find_loop(&grid, start).unwrap();
+    lupe.len() as i32 / 2
 }
 
 pub fn part2(input: &str) -> i64 {
+    let grid = parse(input);
+    let start = find_start(&grid).unwrap();
+    let lupe = find_loop(&grid, start).unwrap();
     0
 }
 
@@ -143,7 +148,6 @@ L|7||
 L|-JF
 ";
         assert_eq!(part1(input), 4);
-        assert_eq!(part2(input), 0);
     }
 
     #[test]
@@ -155,13 +159,75 @@ SJLL7
 LJ.LJ
 ";
         assert_eq!(part1(input), 8);
-        assert_eq!(part2(input), 0);
     }
 
     #[test]
+    fn test_example3() {
+        let input = "...........
+.S-------7.
+.|F-----7|.
+.||OOOOO||.
+.||OOOOO||.
+.|L-7OF-J|.
+.|II|O|II|.
+.L--JOL--J.
+.....O.....
+";
+        assert_eq!(part2(input), 4);
+    }
+
+    #[test]
+    fn test_example4() {
+        let input = "...........
+.S------7.
+.|F----7|.
+.||OOOO||.
+.||OOOO||.
+.|L-7F-J|.
+.|II||II|.
+.L--JL--J.
+..........
+";
+        assert_eq!(part2(input), 4);
+    }
+
+    #[test]
+    fn test_example5() {
+        let input = ".F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ...
+";
+        assert_eq!(part2(input), 8);
+    }
+
+    #[test]
+    fn test_example6() {
+        let input = "FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJ7F7FJ-
+L---JF-JLJ.||-FJLJJ7
+|F|F-JF---7F7-L7L|7|
+|FFJF7L7F-JF7|JL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L
+";
+        assert_eq!(part2(input), 10);
+    }
+
+    #[test]
+    #[ignore]
     fn test_real() {
         let input = include_str!("../inputs/day10.txt");
-        assert_eq!(part1(input), 0);
+        assert_eq!(part1(input), 7102);
         assert_eq!(part2(input), 0);
     }
 }
