176ef3b6d45bed8c48c40f6dd37aff27d4927d1a
Daniel Connelly <dhconnelly@gmail.com>
2023-12-13 07:44:39 +0100

    day13: clean up

--- src/day13.rs
+++ src/day13.rs
@@ -1,38 +1,50 @@
 use crate::static_vec::StaticVec;
-use libc_print::std_name::*;
 
 type Grid = StaticVec<StaticVec<u8, 32>, 32>;
 
-fn parse(pattern: &str) -> Grid {
-    pattern.lines().map(|line| line.bytes().collect()).collect()
+fn count_diffs<'a>(
+    left: impl Iterator<Item = impl Iterator<Item = &'a u8>>,
+    right: impl Iterator<Item = impl Iterator<Item = &'a u8>>,
+) -> usize {
+    left.zip(right).map(|(a, b)| a.zip(b).filter(|(x, y)| x != y).count()).sum()
 }
 
-fn reflection_term(grid: Grid) -> usize {
+fn reflection_term(grid: Grid, diffs: usize) -> usize {
     for row in 0..grid.len() - 1 {
         let num_rows = (grid.len() - row - 1).min(row + 1);
-        let from = row + 1 - num_rows;
-        let to = row + num_rows + 1;
-        if grid[from..row + 1].iter().eq(grid[row + 1..to].iter().rev()) {
-            println!("match: {}-{}-{}", from, row + 1, to);
+        let (from, to) = (row + 1 - num_rows, row + num_rows + 1);
+        let above = grid[from..row + 1].iter().map(|row| row.iter());
+        let below = grid[row + 1..to].iter().map(|row| row.iter()).rev();
+        if count_diffs(above, below) == diffs {
             return (row + 1) * 100;
         }
     }
     for col in 0..grid[0].len() - 1 {
         let num_cols = (grid[0].len() - col - 1).min(col + 1);
-        let from = col + 1 - num_cols;
-        let to = col + num_cols + 1;
-        let a = (from..col + 1).map(|col| grid.iter().map(move |row| row[col]));
-        let b = (col + 1..to).map(|col| grid.iter().map(move |row| row[col]));
-        if a.zip(b.rev()).all(|(a, b)| a.eq(b)) {
-            println!("match col: {}-{}-{}", from, col + 1, to);
+        let (from, to) = (col + 1 - num_cols, col + num_cols + 1);
+        let left = (from..col + 1).map(|col| grid.iter().map(move |row| &row[col]));
+        let right = (col + 1..to).map(|col| grid.iter().map(move |row| &row[col]));
+        if count_diffs(left, right.rev()) == diffs {
             return col + 1;
         }
     }
     0
 }
 
+fn parse(pattern: &str) -> Grid {
+    pattern.lines().map(|line| line.bytes().collect()).collect()
+}
+
+fn summary(input: &str, diffs: usize) -> usize {
+    input.split("\n\n").map(parse).map(|p| reflection_term(p, diffs)).sum()
+}
+
 pub fn part1(input: &str) -> usize {
-    input.split("\n\n").map(parse).map(reflection_term).sum()
+    summary(input, 0)
+}
+
+pub fn part2(input: &str) -> usize {
+    summary(input, 1)
 }
 
 #[cfg(test)]
@@ -58,11 +70,13 @@ mod test {
 #....#..#
 ";
         assert_eq!(part1(input), 405);
+        assert_eq!(part2(input), 400);
     }
 
     #[test]
     fn test_real() {
         let input = include_str!("../inputs/day13.txt");
-        assert_eq!(part1(input), 0);
+        assert_eq!(part1(input), 27502);
+        assert_eq!(part2(input), 31947);
     }
 }
