d6a5b4e842039bfdf2fd2eb98507ca4cf3d1438c
Daniel Connelly <dhconnelly@gmail.com>
2023-12-14 07:19:01 +0100

    day14: part1

--- src/day14.rs
+++ src/day14.rs
@@ -1,5 +1,88 @@
-pub fn part1(input: &str) -> i64 {
-    0
+use crate::static_vec::StaticVec;
+use libc_print::std_name::*;
+
+#[derive(Clone, Copy, Default)]
+enum Tile {
+    #[default]
+    Empty,
+    Round,
+    Cube,
+}
+
+impl core::fmt::Debug for Tile {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        let c = match self {
+            Tile::Empty => '.',
+            Tile::Round => 'O',
+            Tile::Cube => '#',
+        };
+        write!(f, "{}", c)
+    }
+}
+
+impl Tile {
+    fn load(&self) -> usize {
+        match self {
+            Tile::Round => 1,
+            _ => 0,
+        }
+    }
+}
+
+impl From<u8> for Tile {
+    fn from(value: u8) -> Self {
+        match value {
+            b'.' => Self::Empty,
+            b'#' => Self::Cube,
+            b'O' => Self::Round,
+            _ => panic!("invalid tile"),
+        }
+    }
+}
+
+type Grid = StaticVec<StaticVec<Tile, 128>, 128>;
+
+fn parse(input: &str) -> Grid {
+    input.lines().map(|line| line.bytes().map(Tile::from).collect()).collect()
+}
+
+fn roll_north(grid: &mut Grid) {
+    for col in 0..grid[0].len() {
+        let mut avail = 0;
+        for row in 0..grid.len() {
+            match grid[row][col] {
+                Tile::Round if avail == row => avail = row + 1,
+                Tile::Cube => avail = row + 1,
+                Tile::Round => {
+                    (grid[avail][col], grid[row][col]) = (Tile::Round, Tile::Empty);
+                    avail += 1;
+                }
+                Tile::Empty => {}
+            }
+        }
+    }
+}
+
+fn print_grid(grid: &Grid) {
+    for row in grid.iter() {
+        println!("{:?}", row);
+    }
+    println!();
+}
+
+fn total_load(grid: &Grid) -> usize {
+    grid.iter()
+        .enumerate()
+        .map(|(i, row)| row.iter().map(move |tile| (grid.len() - i) * tile.load()))
+        .flatten()
+        .sum()
+}
+
+pub fn part1(input: &str) -> usize {
+    let mut grid = parse(input);
+    print_grid(&grid);
+    roll_north(&mut grid);
+    total_load(&grid)
 }
 
 pub fn part2(input: &str) -> i64 {
@@ -12,8 +95,18 @@ mod test {
 
     #[test]
     fn test_examples() {
-        let input = "";
-        assert_eq!(part1(input), 0);
+        let input = "O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#....
+";
+        assert_eq!(part1(input), 136);
         assert_eq!(part2(input), 0);
     }
 
