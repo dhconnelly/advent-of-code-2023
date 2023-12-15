6aaeb92d2b1effc5646b7cf2a18ab2a742db422d
Daniel Connelly <dhconnelly@gmail.com>
2023-12-15 07:20:03 +0100

    day15: part1, parse op for part 2

--- src/day15.rs
+++ src/day15.rs
@@ -1,3 +1,5 @@
+use crate::static_vec::StaticVec;
+
 fn hash(s: &str) -> u8 {
     s.bytes().fold(0u16, |cur, b| (cur + b as u16) * 17 % 256) as u8
 }
@@ -6,7 +8,27 @@ pub fn part1(input: &str) -> i64 {
     input.split(',').map(str::trim).map(hash).map(|h| h as i64).sum()
 }
 
+#[derive(Debug)]
+enum Op<'a> {
+    Insert(&'a str, u8),
+    Remove(&'a str),
+}
+
+fn parse_op(s: &str) -> Op {
+    let i = s.find(|c| c == '-' || c == '=').unwrap();
+    let (lens, tail) = (&s[..i], &s[i..]);
+    if tail.starts_with('-') {
+        Op::Remove(lens)
+    } else {
+        Op::Insert(lens, tail[1..].parse().unwrap())
+    }
+}
+
 pub fn part2(input: &str) -> i64 {
+    for tok in input.split(',').map(str::trim) {
+        let op = parse_op(tok);
+        libc_print::libc_println!("{:?}", op);
+    }
     0
 }
 
