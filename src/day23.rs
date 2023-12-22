665196725c6d1bd292bfc376e63aaf0140ae8786
Daniel Connelly <dhconnelly@gmail.com>
2023-12-22 20:01:03 +0100

    day23: scaffolding

---
+++ src/day23.rs
@@ -0,0 +1,26 @@
+pub fn part1(input: &str) -> usize {
+    0
+}
+
+pub fn part2(input: &str) -> usize {
+    0
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_example() {
+        let input = "";
+        assert_eq!(part1(input), 0);
+        assert_eq!(part2(input), 0);
+    }
+
+    #[test]
+    fn test_real() {
+        let input = "";
+        assert_eq!(part1(input), 0);
+        assert_eq!(part2(input), 0);
+    }
+}
