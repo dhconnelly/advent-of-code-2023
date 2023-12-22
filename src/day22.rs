3f1050afddc996d667c93160d6e0a0330322d22b
Daniel Connelly <dhconnelly@gmail.com>
2023-12-22 10:28:46 +0100

    day22: part2 but slow

--- src/day22.rs
+++ src/day22.rs
@@ -1,5 +1,5 @@
 use core::ops::Deref;
-use heapless::Vec;
+use heapless::{FnvIndexSet, Vec};
 use libc_print::std_name::*;
 
 type Bricks = Vec<Brick, 2048>;
@@ -13,6 +13,7 @@ struct Pt {
 }
 
 const ZERO: Pt = Pt { x: 0, y: 0, z: 0 };
+const REMOVED: Brick = (ZERO, ZERO);
 
 fn intersects_xy(l: &Brick, r: &Brick) -> bool {
     let ix = (l.0.x.max(r.0.x), l.1.x.min(r.1.x));
@@ -72,49 +73,65 @@ fn drop_dist(bricks: &Bricks, i: usize) -> i16 {
         .unwrap_or(brick.0.z - 1)
 }
 
-fn drop(bricks: &mut Bricks, i: usize) {
+fn drop(bricks: &mut Bricks, i: usize) -> bool {
     let dz = drop_dist(bricks, i);
     if dz > 0 {
         bricks[i].0.z -= dz;
         bricks[i].1.z -= dz;
     }
+    dz > 0
 }
 
-fn drop_all(bricks: &mut Bricks) {
-    for i in 0..bricks.len() {
-        drop(bricks, i);
-    }
+fn drop_all(bricks: &mut Bricks) -> usize {
+    (0..bricks.len()).map(|i| drop(bricks, i) as usize).sum()
 }
 
-fn fall_count(bricks: &mut Bricks, i: usize) -> usize {
+fn supported(bricks: &mut Bricks, i: usize) -> FnvIndexSet<usize, 2048> {
     let brick = bricks[i];
     // look for a brick that would fall on this one
-    let mut fall_count = 0;
+    let mut supported = FnvIndexSet::new();
     for j in i + 1..bricks.len() {
         let other = bricks[j];
         if other.0.z <= brick.1.z || !intersects_cached(i, j) {
             continue;
         }
-        bricks[i] = (ZERO, ZERO);
+        bricks[i] = REMOVED;
         // if it doesn't fall, we can remove
         let d = drop_dist(bricks, j);
         bricks[i] = brick;
         if d > 0 {
-            fall_count += 1;
+            supported.insert(j).unwrap();
         }
     }
-    fall_count
+    supported
 }
 
 pub fn part1(input: &str) -> usize {
     let mut bricks = parse(input);
     compute_intersections(&bricks);
     drop_all(&mut bricks);
-    (0..bricks.len()).filter(|i| fall_count(&mut bricks, *i) == 0).count()
+    (0..bricks.len()).filter(|i| supported(&mut bricks, *i).len() == 0).count()
 }
 
-pub fn part2(input: &str) -> i64 {
-    0
+fn count_supporting(bricks: &mut Bricks, i: usize, memo: &mut [Option<usize>]) -> usize {
+    if let Some(count) = memo[i] {
+        return count;
+    }
+    let mut next = bricks.clone();
+    next[i] = REMOVED;
+    let count = drop_all(&mut next);
+    memo[i] = Some(count);
+    println!("would fall if {} were removed: {}", i, count);
+    count
+}
+
+pub fn part2(input: &str) -> usize {
+    let mut bricks = parse(input);
+    drop_all(&mut bricks);
+    compute_intersections(&bricks);
+    let mut memo: Vec<Option<usize>, 2048> = Vec::new();
+    memo.resize(bricks.len(), None).unwrap();
+    (0..bricks.len()).map(|i| count_supporting(&mut bricks, i, &mut memo)).sum()
 }
 
 #[cfg(test)]
@@ -132,7 +149,7 @@ mod test {
 1,1,8~1,1,9
 ";
         assert_eq!(part1(input), 5);
-        assert_eq!(part2(input), 0);
+        assert_eq!(part2(input), 7);
 
         let input = include_str!("../inputs/day22.txt");
         assert_eq!(part1(input), 403);
